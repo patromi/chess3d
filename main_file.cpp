d704f2f13686063cd617c71cdd58a69b269bc2c9
admin <patromi123@gmail.com>
2025-05-26 22:04:13 +0200

    Revert "patromi - pgn-reader"
    This reverts commit 98d62e97dfe481220a420640116e4a42b27f0b50.

--- main_file.cpp
+++ main_file.cpp
@@ -34,11 +34,6 @@ Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 #include "myTeapot.h"
 
 #include "Model.h"
-#include <iostream>
-#include <string>
-
-// Deklaracja funkcji z gameLogic.cpp
-void readPGN(const std::string& pgnString);
 
 
 float speed_x = 0;
@@ -210,16 +205,6 @@ void drawScene(GLFWwindow* window, float angle_x, float angle_y) {
 
 int main(void)
 {
-	std::string pgn =
-		"1.e4 e5 2.Nf3 Nc6 3.Bb5 a6 4.Ba4 Nf6 5.O-O Be7 6.Re1 b5 7.Bb3 d6 8.c3 O-O "
-		"9.h3 Nb8 10.d4 Nbd7 11.c4 c6 12.cxb5 axb5 13.Nc3 Bb7 14.Bg5 b4 15.Nb1 h6 "
-		"16.Bh4 c5 17.dxe5 Nxe4 18.Bxe7 Qxe7 19.exd6 Qf6 20.Nbd2 Nxd6 21.Nc4 Nxc4 "
-		"22.Bxc4 Nb6 23.Ne5 Rae8 24.Bxf7+ Rxf7 25.Nxf7 Rxe1+ 26.Qxe1 Kxf7 27.Qe3 "
-		"Qg5 28.Qxg5 hxg5 29.b3 Ke6 30.a3 Kd6 31.axb4 cxb4 32.Ra5 Nd5 33.f3 Bc8 "
-		"34.Kf2 Bf5 35.Ra7 g6 36.Ra6+ Kc5 37.Ke1 Nf4 38.g3 Nxh3 39.Kd2 Kb5 "
-		"40.Rd6 Kc5 41.Ra6 Nf2 42.g4 Bd3 43.Re6 1/2-1/2";
-
-	readPGN(pgn);
 	GLFWwindow* window; //Wskaźnik na obiekt reprezentujący okno
 
 	glfwSetErrorCallback(error_callback);//Zarejestruj procedurę obsługi błędów
@@ -267,8 +252,5 @@ int main(void)
 
 	glfwDestroyWindow(window); //Usuń kontekst OpenGL i okno
 	glfwTerminate(); //Zwolnij zasoby zajęte przez GLFW
-
-
-
 	exit(EXIT_SUCCESS);
 }
